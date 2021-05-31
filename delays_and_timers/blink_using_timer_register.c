0695e1dacd8ecea636b551f2a6f115d79735002b
William W Clark <wclark@pitt.edu>
2021-05-31 14:10:17 -0400

    Changes LED toggling and variable in wait function

--- delays_and_timers/blink_using_timer_register.c
+++ delays_and_timers/blink_using_timer_register.c
@@ -32,24 +32,26 @@ int main(void)
    while(1)
     {
         // The main loop -- toggle the bit, then delay to set the frequency
-        PORTC = PORTC ^ 1<<PORTC0;  // Note that this uses the Exclusive OR operator, which toggles an individual 
-			// bit without changing the other bits in PORTC
-                
-        wait(250); // call delay function: the wait function will loop 250 time using delay #2 (1 msec each loop) for a total of 250 msec delay
-         
-    }
-	
+		PORTC = 0b00000001; // Turn off LED -- note that this also clears all other bits on PORTC
+		wait(250);
+		PORTC = 0b00000000; // Turn on LED --  note that this also clears all other bits on PORTC
+		wait(250);
+		
+		// An alternate and efficient way to toggle the LED on and off is shown below with single bit addressing.  This avoids affecting other bits on PORTC besides the one we want to address.
+        //PORTC = PORTC ^ 1<<PORTC0;  // Note that this uses the Exclusive OR operator, which toggles an individual bit without changing the other bits in PORTC
+        //wait(250); // call delay function: the wait function will loop 250 time using delay #2 (1 msec each loop) for a total of 250 msec delay     
+    }	
 	return(0);
 } // end main()
 
-void wait(volatile int multiple) {
-	// This subroutine creates a delay equal to multiple*T, where T is 1 msec
-	while (multiple > 0) {
+void wait(volatile int N) {
+	// This subroutine creates a delay equal to N msec
+	while (N > 0) {
 		TCCR0A = 0x00; // clears WGM00 and WGM01 (bits 0 and 1) to ensure Timer/Counter is in normal mode.
 		TCNT0 = 0;  // preload value for testing on count = 250
-		TCCR0B =  0b00000011; //1<<CS01 | 1<<CS00;	TCCR0B = 0x03;  // Start TIMER0, Normal mode, crystal clock, prescaler = 64
+		TCCR0B =  0b00000011; //1<<CS01 | 1<<CS00;	TCCR0B = 0x03;  // Start TIMER0, Normal mode, 16 MHz crystal clock, prescaler = 64
 		while (TCNT0 < 0xFA); // exits when count = 250 (requires preload of 0 to make count = 250)
 		TCCR0B = 0x00; // Stop TIMER0
-		multiple--;
+		N--;
 	}
 } // end wait()
