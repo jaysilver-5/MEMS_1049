cc7edd6bb62b1eab102dc4a8b670427e126f33bf
William W Clark <wclark@pitt.edu>
2021-05-31 14:56:08 -0400

    Changed comment on while in wait function to make clearer

--- delays_and_timers/blink_using_timer_register.c
+++ delays_and_timers/blink_using_timer_register.c
@@ -50,7 +50,7 @@ void wait(volatile int N) {
 		TCCR0A = 0x00; // clears WGM00 and WGM01 (bits 0 and 1) to ensure Timer/Counter is in normal mode.
 		TCNT0 = 0;  // preload value for testing on count = 250
 		TCCR0B =  0b00000011; //1<<CS01 | 1<<CS00;	TCCR0B = 0x03;  // Start TIMER0, Normal mode, 16 MHz crystal clock, prescaler = 64
-		while (TCNT0 < 0xFA); // exits when count = 250 (requires preload of 0 to make count = 250)
+		while (TCNT0 < 0xFA); // exits when count = 250 (which is the count required to create a 1 msec delay with 16MHz clock and prescaler of 64)
 		TCCR0B = 0x00; // Stop TIMER0
 		N--;
 	}
